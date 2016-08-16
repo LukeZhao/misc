dd0819d66db5a1b8deb3ea246162981e82a34ed8
Luke Zhao <lzhao@telecomsys.com>
2016-08-16 21:31:08 +0000

    update

--- finduser.py
+++ finduser.py
@@ -6,14 +6,36 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from connections import switch_customer
 from models import Consultation
 from main import create_app
+from datetime import datetime
+from integration import aws_ses
 customer = sys.argv[1] if len(sys.argv) > 1 else 'weppa'
-
+email_list = ('sam.sager@relymd.com',
+              'matthew.cox@relymd.com',
+              'david.kammer@relymd.com',
+              'Anthony.Araujo@comtechtel.com',
+              'maggie.wu@comtechtel.com',
+              'zlata.koro@comtechtel.com',
+              'Bruce.Rethwisch@comtechtel.com')
+test_email = ('qingbo_zhao@hotmail.com',)
 app = create_app(schedule_events=False)
 app.app_context().push()
-switch_customer(customer) or exit()
-print 'first_name\tlast_name\temail'
-for con in Consultation.objects(state='pre_queue'):
-    print '{}\t{}\t{}'.format(con.patient.first_name,
-                              con.patient.last_name,
-                              con.patient.email)
-sys.stdout.flush()
+
+def main():
+    subject = 'pre_queue users for relymd: {}'.format(datetime.today().date())
+    switch_customer(customer) or exit()
+    body = 'first_name\tlast_name\temail\tcreated\n\n'
+    for con in Consultation.objects(state='pre_queue'):
+        body = body + '{}\t{}\t{}\t{}\n'.format(con.patient.first_name,
+                                  con.patient.last_name,
+                                  con.patient.email,
+                                  con.created)
+    for email in test_email: #email_list:
+        try:
+            aws_ses.send_message(email, subject, body)
+        except:
+            pass
+
+if __name__ == '__main__':
+    main()
+    exit()
+
