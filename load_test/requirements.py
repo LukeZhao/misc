63dc26a5cf00510ef57dc5c2a10b502c18b03b5a
LukeZhao <lzhao@telecomsys.com>
2016-10-03 20:15:50 +0000

    added new files

---
+++ load_test/requirements.py
@@ -0,0 +1,15 @@
+Flask==0.10.1
+Jinja2==2.7.3
+MarkupSafe==0.23
+Werkzeug==0.10.4
+gevent==1.1b6
+greenlet==0.4.9
+ipdb==0.8
+ipython==3.1.0
+itsdangerous==0.24
+locustio==0.7.2
+msgpack-python==0.4.6
+pyzmq==14.5.0
+requests==2.6.0
+wsgiref==0.1.2
+
